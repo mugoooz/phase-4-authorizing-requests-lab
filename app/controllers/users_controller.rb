da00883444cc8e6981b8dd598e21bcb452f0af40
Eric Mugo <mugoericpg20@gmail.com>
2023-06-28 15:18:23 +0300

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,29 @@
 class UsersController < ApplicationController
-
-  def show
-    user = User.find(session[:user_id])
-    render json: user
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
+  def show
+    if logged_in?
+      render json: current_user
+    else
+      render_unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+
+  def render_unauthorized
+    render json: { error: 'Unauthorized access' }, status: :unauthorized
+  end
 end
